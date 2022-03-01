7b6f20e83d553fee4221704b7bc202fb039d1a82
root <root@DESKTOP-LCA5644.localdomain>
2022-03-01 19:23:09 +0530

    feat: episode 7 solution

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,24 +1,29 @@
-const webpack = require('webpack')
-const { defineConfig } = require('@vue/cli-service')
+const webpack = require("webpack")
+const { defineConfig } = require("@vue/cli-service")
+const path = require('path')
 
 module.exports = defineConfig({
-    transpileDependencies: true,
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                Buffer: ['buffer', 'Buffer']
-            })
-        ],
-        resolve: {
-            fallback: {
-                crypto: false,
-                fs: false,
-                assert: false,
-                process: false,
-                util: false,
-                path: false,
-                stream: false,
-            }
-        }
-    }
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ],
+    resolve: {
+      fallback: {
+        crypto: false,
+        fs: false,
+        assert: false,
+        process: false,
+        util: false,
+        path: false,
+        stream: false,
+      },
+    },
+  },
+  chainWebpack(config) {
+    config.resolve.symlinks(false)
+    config.resolve.alias.set("vue", path.resolve("./node_modules/vue"))
+  },
 })
