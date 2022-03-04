54a02ae135094dc80aaafe27a4bb8f99ab89f36e
root <root@DESKTOP-LCA5644.localdomain>
2022-03-04 17:07:04 +0530

    feat: episode 10 solution

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -6,13 +6,15 @@ import idl from '../../../target/idl/solana_twitter.json'
 
 const programID = new PublicKey(idl.metadata.address)
 let workspace = null
+const preflightCommitment = 'processed'
+const commitment = 'processed'
 
 export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899')
-    const provider = computed(() => new Provider(connection, wallet.value))
+    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
     workspace = {
